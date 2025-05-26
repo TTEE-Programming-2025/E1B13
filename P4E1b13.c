16bc2520f7c40b72b13cb1ae544d8b4aa13b9875
unknown <u11302213@o365.ttu.edu.tw>
2025-05-26 23:06:06 +0800

    add the inprect and the thoughts .the homework4 is done.

--- P4E1b13.c
+++ P4E1b13.c
@@ -3,20 +3,18 @@
 #include<time.h>
 #include <conio.h>
 #include<windows.h>
-struct studentdata{
+struct studentdata{// set the struct
 	char name[15];
 	char  studentid[6];
 	short int math;
 	short int english;
-	short int physics;
+	short int physics;// short int is number insite the two bytes
 	float average;
 } ;
-int student(int,struct studentdata *class2 );
-void printdata(struct studentdata *class2,int);
-void search(struct studentdata *class2,char *,int);
-void graderank(struct studentdata *class2,int);
-void setseat(char );
-char seats [9][9];
+int student(int,struct studentdata *class2 );//a function
+void printdata(struct studentdata *class2,int);//b function
+void search(struct studentdata *class2,char *,int);//c function
+void graderank(struct studentdata *class2,int);//d function
 int main(void)
 {
 	int i,j,num,tryno,num1,n; 
@@ -26,7 +24,7 @@ int main(void)
 	time_t seconds ;
 	for (i=0;i<=10;i++){
 		printf("**********************************************************\n");
-	}      //印出10次符號 
+	}      //Print 10 symbols
 	printf("**                                                      **\n");
 	printf("**       jimmy's program 4                              **\n");
 	printf("**       E1B13                                          **\n");
@@ -34,10 +32,10 @@ int main(void)
 	for (i=0;i<=6;i++){
 		printf("**********************************************************\n");
 		printf("##########################################################\n");
-    }     //印出7次2行交錯符號 
+    }     //Print 7 times 2 lines of staggered symbols
 	printf("**                              2025.5.24製作            **\n");
-	system("pause");//等待使用者輸入 
-	system("cls");//清除螢幕
+	system("pause");//Waiting for user input 
+	system("cls");//Clear Screen
 	printf("please write the four code:");
 	scanf("%d",&num);
 	if(num!=2025){
@@ -46,22 +44,22 @@ int main(void)
 			printf("%d is error ,this is %d time enter\n",num,tryno);
 			printf("please write the four code:");
 			scanf("%d",&num); 
-			if(num==2025)break;//3次以內密碼正確就跳出這for迴圈 
+			if(num==2025)break;//If the password is correct within 3 times, jump out of this for loop
 			else if (num!=2025){
 				tryno++;
 				if(i==3){
 					printf("%d is error ,this is %d time enter",num,tryno);
 	
 				}
-				continue;//如果錯第3次就會先顯示密碼錯誤,在結束程式 
+				continue;//If you make a mistake for the third time, it will display the password error first, then end the program
 			}
 		}
 	} 
 	if(num==2025){
 		printf("%d is correct",num);
 		while(1){
-		 	Sleep(500);//延遲5秒  
-			system("cls");//清除螢幕 
+		 	Sleep(500);//Delay 5 seconds 
+			system("cls");//Clear Screen 
 			printf("----[Grade System]---------------\n");
 			printf("|    a.Enter student grades     |\n");
 			printf("|                               |\n");
@@ -76,47 +74,54 @@ int main(void)
 			printf("please write the characters abcde  :");
 	   		scanf(" %c",&ch1);
 	   		switch(ch1){
+	   			//exercise 3
 				case 'a':
 					system("cls");
-					fflush(stdin);
+					fflush(stdin);//Clear the input buffer
 					printf("please input the student no. n (range 5-10)");
 					scanf("%d",&n);
-					count=student(n,class1);
-					system("pause");
-					system("cls");
+					count=student(n,class1);//Passing values to student  functions
+					system("pause");//wating user enter
+					system("cls");//clean the screen
 					break;
+				// exercise 4
 				case 'b':
 					system("cls");	
-					printdata(class1,count);
-					system("pause");
+					printdata(class1,count);//Passing values to printdata  functions
+					system("pause");//wating user enter
 					system("cls");
 					break;
+				// exercise 5
 				case 'c':
 					system("cls");
 					fflush(stdin);
 					printf("please input the serch name:");	
-					gets(serchname);
+					gets(serchname);//Read the string input by the user
 					printf("serch name=%s\n",serchname);
-					search(class1,serchname,count);
+					search(class1,serchname,count);//Passing values to serchname  functions
 					system("pause");
 					system("cls");
 					break;
+				// exercise 6
 				case 'd' :
-					system("cls");
-					graderank(class1,count);
-					system("pause");
+					system("cls");//clean the screen
+					graderank(class1,count);//Passing values to graderank  functions
+					system("pause");//wating user enter
 					system("cls");
 					break;
+				// exercise 7
 				case'e':
-					system("cls");
-					fflush(stdin);
+					system("cls");//clean the screen
+					fflush(stdin);//Clear the input buffer
 					printf("Are you sure to leave?");
 					scanf("%c",&u);
+					//If the input is not y or n, an error message will be displayed 
+					//and you will be prompted to re-enter the
 					while(u!='y'&&u!='n'){
 						printf("the enter is error\n");
-						fflush(stdin);
+						fflush(stdin);//Clear the input buffer
 						printf("Are you sure to leave?");
-						scanf("%c",&u);
+						scanf("%c",&u); 
 					}
 					if (u=='n'){
 						break;				 
@@ -131,24 +136,31 @@ int main(void)
 	return 0;
 	}	
 } 
+//exercise 3 function 
 int student(int r,struct studentdata *class2){
 
 	int i,k,flag=0;
 	char id[15];
 	float average;
+	// When the input range is not 5-10, 
+	//an error message will be displayed and you will be prompted to re-enter the number.
 	while(r<5||r>10){
 		printf("not range number\n");
 		printf("please write the number n (range 5-10)");
 		scanf("%d",&r);
 	}
+	// Use the number you just entered as the number 
+	//and enter the name, ID, and score in order until that number is reached.
 	for(i=0;i<r;i++){
 		printf("please enter the %d students name\n",i+1);
 		scanf("%s",(class2+i)->name);
 		fflush(stdin);
 		printf("please enter the %d students Id\n",i+1);
 		gets(id);
+		//Change the number to a string to prevent 
+		//the 6-digit number starting with 0 from not being displayed
 		while(1){
-			
+			//When the string length is not 6, an error message will be displayed
 			while((strlen(id))!=6){
 				printf("The range of students ID is wrong.\n");
 				printf("please enter the %d students Id.\n",i+1);
@@ -156,10 +168,12 @@ int student(int r,struct studentdata *class2){
 
 			}
 			k=(strlen(id));
+			//Check if there are other characters in the 6-digit number.
+			// If there are  not, it will display an error and you need to re-enter the number.
 			for(k=0;k<=5;k++){
 				if(id[k]>=48 && id[k]<=57){
 					if (k==5) {
-						flag = 1	;
+						flag = 1	;// if there are,copy the id to the structure
 					} 
 				}
 				else{
@@ -171,18 +185,22 @@ int student(int r,struct studentdata *class2){
 				
 			}
 			if(flag==1){
-				strcpy((class2+i)->studentid,id);
+				strcpy((class2+i)->studentid,id);//Copy the id to the structure
 				flag=0;
 				break;
 			}
 		}
 		printf("please enter the %d students math grade\n",i+1);
 		scanf("%d",&(class2+i)->math);
+		//If the range is not 1-100, 
+		//an error message will be displayed and you will be asked to re-enter the value.
 		while((class2+i)->math>100||(class2+i)->math<0){
 			printf("wrong numer range\n");
 			printf("please enter the %d students math grade\n",i+1);
 			scanf("%d",&(class2+i)->math);
 		}
+		//If the range is not 1-100, 
+		//an error message will be displayed and you will be asked to re-enter the value.
 		printf("please enter the %d students english grade\n",i+1);
 		scanf("%d",&(class2+i)->english);
 		while((class2+i)->english>100||(class2+i)->english<0){
@@ -197,12 +215,16 @@ int student(int r,struct studentdata *class2){
 			printf("please enter the %d students physics grade\n",i+1);
 			scanf("%d",&(class2+i)->physics);
 		}
+		//Add all of them and divide by 3.0 to get the average value. 
+		//The 0 is to have a decimal place at the end.
 		(class2+i)->average=((class2+i)->math + (class2+i)->english + (class2+i)->physics)/3.0;
 	}
 	return r;
 }
+//exercise 4 function
 void printdata(struct studentdata *class2,int j){
 	int i=0;
+		//List the content you just entered one by one
 		for(i=0;i<j;i++){
 			printf("NO%d name=%s\n",i+1,(class2+i)->name);
 			printf("NO%d Student ID=%s\n",i+1,(class2+i)->studentid);
@@ -213,9 +235,11 @@ void printdata(struct studentdata *class2,int j){
 		}
 		
 }
+//exercise 5 function
 void search(struct studentdata *class2,char *sname,int j){
 	int i = 0,sflag=0;
 	for(i=0;i<j;i++){
+		//If the name entered is in the structure, the ID of the name is displayed.
 		if(strcmp((class2+i)->name,sname)==0){
 			printf("NO%d name=%s\n",i+1,(class2+i)->name);
 			printf("NO%d Student ID=%s\n",i+1,(class2+i)->studentid);
@@ -227,9 +251,11 @@ void search(struct studentdata *class2,char *sname,int j){
 			break ;
 		}
 	}
+	//If no input is given, an error message will be displayed.
 	if (sflag==0)
 		printf("Can't find the name=%s,the name dosen't exist!",sname)	;
 }
+//exercise 6 function
 void graderank(struct studentdata *class2,int j){
 	int i=0,k=0,sortflag;
 	float tempmin;
@@ -241,13 +267,14 @@ void graderank(struct studentdata *class2,int j){
 				temp=class2[k];
 				class2[k]=class2[k+1];
 				class2[k+1]=temp;
-				sortflag=1;//目的看有沒有排序過 
+				sortflag=1;//The purpose is to see if it has been sorted
 			}
-		}  //如果不用排序就跳脫迴圈 
+		}  //If you don't need to sort, you can break out of the loop.
 		if(sortflag==0){
 			break;
 		}
 	}
+	//Print the names in order of grades.
 	for(i=j-1;i>=0;i--){
 			printf("NO%d name=%s\n",i+1,(class2+i)->name);
 			printf("NO%d Student ID=%s\n",i+1,(class2+i)->studentid);
@@ -257,4 +284,14 @@ void graderank(struct studentdata *class2,int j){
 			printf("NO%d Average=%.1f\n",i+1,(class2+i)->average);	
 	}
 }
-
+/*This homework, I conducted basic exercises on pointers and structures in C language, 
+which gave me a deeper understanding of these two very critical concepts in C language.
+this homework The difficult part of this assignment was the structure's formula transmission and structure's formula setting. 
+I spent a lot of time thinking about these two formulas. 
+Finally, I solved it with the help of my father's explanation and C language books. 
+I get help from my father almost every time I do programming assignments. 
+I am very grateful that he takes the time to help me every time.
+In summary, this exercise not only helped me learn grammar, but more importantly, 
+it helped me establish a basic understanding of the underlying memory operations. 
+This is very helpful for further learning about dynamic memory allocation and data structures  in the future. 
+*/
